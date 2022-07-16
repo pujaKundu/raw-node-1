125c7c23949a82bf95929f441b8dbcfe1c873b2b
pujaKundu <iampujakundu@gmail.com>
2022-07-16 12:02:55 +0600

    code

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 //dependecies
 const http = require("http");
-const url = require("url");
-const { StringDecoder } = require("string_decoder");
+
+const { handleReqRes } = require("./helpers/handleReqRes");
+
+
 
 // app object
 const app = {};
@@ -10,6 +12,8 @@ const app = {};
 app.config = {
   port: 3000,
 };
+
+const chosenHandler = routes[tr]
 
 //create server
 app.createServer = () => {
@@ -20,32 +24,6 @@ app.createServer = () => {
 };
 
 // request response
-app.handleReqRes = (req, res) => {
-  //get the url and parse it
-  const parsedUrl = url.parse(req.url, true);
-  const path = parsedUrl.pathname;
-  //console.log(parsedUrl)
-  const trimmedPath = path.replace(/^\/*|\/+$/g, "");
-  //console.log(trimmedPath);
-  const method = req.method.toLowerCase();
-  const queryStringObject = parsedUrl.query;
-  //console.log(queryStringObject)
-  const headersObject = req.headers;
-
-  const decoder = new StringDecoder("utf-8");
-  let realData = "";
-
-  req.on("data", (buffer) => {
-    realData += decoder.write(buffer);
-  });
-    
-    req.on('end', () => {
-        realData += decoder.end();
-        console.log(realData);
-        res.end("Hello World")
-  })
-
- 
-};
+app.handleReqRes = handleReqRes
 
 app.createServer();
